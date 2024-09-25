5c0407c4a165e8bb55533dcb1127b3444e0a6915
aidenwang23 <148901698+aidenwang23@users.noreply.github.com>
2024-09-25 15:20:16 +0000

    modified:   Feeder.java 	modified:   Main.java

--- Main.java
+++ Main.java
@@ -1,11 +1,20 @@
 public class Main {
     public static void main(String[] args){
-        Feeder i = new Feeder(200);
-        i.simulateOneDay(5);
-        System.out.println(i);
+        Feeder a = new Feeder(500);
+        a.simulateOneDay(12);
+        System.out.println(a);
+        Feeder b = new Feeder(1000);
+        b.simulateOneDay(22);
+        System.out.println(b);
+        Feeder c = new Feeder(100);
+        c.simulateOneDay(5);
+        System.out.println(c);
 
-        Feeder j = new Feeder(1000);
-        j.simulateManyDays(5, 5);
-        System.out.println(j);
+        Feeder d = new Feeder(2400);
+        System.out.println(d.simulateManyDays(10,4));
+        Feeder e = new Feeder(250);
+        System.out.println(e.simulateManyDays(10, 5));
+        Feeder f = new Feeder(0);
+        System.out.println(f.simulateManyDays(5, 10));
     }
 }
